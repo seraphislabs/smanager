1d316f367039507b71e7b37f6208dfa60dcbe7e6
seraphislabs <seraphislabs@gmail.com>
2023-06-03 21:47:05 +0000

    Latest Stable

--- engine/databaseinterface.php
+++ engine/databaseinterface.php
@@ -2,18 +2,83 @@
 
 class PDODatabase
 {
-    private $connection;
+    private $host;
+    private $username;
+    private $password;
+    private $dbname;
+    private $pdo;
 
-    public function connect($host, $username, $password, $database)
+    public function __construct($host, $username, $password, $dbname)
     {
-        $dsn = "mysql:host=$host;dbname=$database;charset=utf8mb4";
+        $this->host = $host;
+        $this->username = $username;
+        $this->password = $password;
+        $this->dbname = $dbname;
+        $this->connect();
+    }
+
+    private function connect()
+    {
+        $dsn = "mysql:host={$this->host};dbname={$this->dbname}";
 
         try {
-            $this->connection = new PDO($dsn, $username, $password);
-            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $this->connection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+            $this->pdo = new PDO($dsn, $this->username, $this->password);
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
             die("Connection failed: " . $e->getMessage());
+        }
+    }
+
+    public function ValidateLogin($_email, $_password) {
+        // Retreive password hash from database
+			$retVal = $this->GetLoginPasswordHash($_email);
+			if (!empty($retVal)) {
+				if (PasswordEncrypt::Check($_password, $retVal)) {
+					return true;
+				}
+			}
+
+            return false;
+    }
+
+    function GetLoginPasswordHash($_email) {
+        $stmt = $this->pdo->prepare("SELECT * FROM `users` WHERE `email`=:email");
+        $stmt->bindParam(":email", $_email);
+        $stmt->execute();
+        if ($stmt->rowCount() == 1) {
+            $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            $passHash = $results[0]['password'];
+
+            return $passHash;
+        }
+    }
+
+    public function GetLoginCompanyID($_email, $_password) {
+        if ($this->ValidateLogin($_email, $_password)) {
+            $stmt = $this->pdo->prepare("SELECT * FROM `users` WHERE `email`=:email");
+            $stmt->bindParam(":email", $_email);
+            $stmt->execute();
+            if ($stmt->rowCount() == 1) {
+                $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+                $companyId = $results[0]['companyid'];
+
+                return $companyId;
+            }
+        }
+    }
+
+    public function GetLoginInformation($_email, $_password) {
+        if ($this->ValidateLogin($_email, $_password)) {
+            $stmt = $this->pdo->prepare("SELECT * FROM `users` WHERE `email`=:email");
+            $stmt->bindParam(":email", $_email);
+            $stmt->execute();
+            if ($stmt->rowCount() == 1) {
+                $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+                return $results[0];
+            }
         }
     }
 }
--- engine/engine.php
+++ engine/engine.php
@@ -1,28 +1,38 @@
 <?php
 	session_start();
-	require("/nginx/protectedfiles/config.php");
-	require_once('databaseinterface.php');
+	require('databaseinterface.php');
+	require_once("/nginx/protectedfiles/config.php");
 	require('user.php');
 	require('utils.php');
 
-	function Action_CheckSession() {
-		$isLogged = true;
-		if (!isset($_SESSION['email']) || !isset($_SESSION['password']) || !isset($_SESSION['companyid'])) {
-			$isLogged = false;
+	function Action_CheckSession($_dbInfo) {
+		$isLogged = false;
+		if (isset($_SESSION['email']) && isset($_SESSION['password'])) {
+			$retVal = $_dbInfo['SMDATA']->ValidateLogin($_SESSION['email'], $_SESSION['password']);
+			echo $retVal ? 'true' : 'false';
 		}
-
-		return $isLogged ? 'true' : 'false';
+		else {
+			echo ("false");
+		}
 	}
 
-	function Action_InitPortal() {
+	function Action_InitPortal($_dbInfo) {
+		$userInfo = $_dbInfo['SMDATA']->GetLoginInformation($_SESSION['email'], $_SESSION['password']);
+		$_SESSION['companyid'] = $userInfo['companyid'];
+		$_SESSION['userid'] = $userInfo['id'];
+
+		$finalID = $userInfo['companyid'] + 1000;
+
+		$tempPDO = new PDODatabase('localhost', $_dbInfo['dusername'], $_dbInfo['dpassword'], "company_$finalID");
+
 		echo("<div id='topbar_container' class='outline'>");
-		echo("<div class='sitelogo'>Service <div class='color1'>Manager</div></div>");
+		echo("<div class='sitelogo'>Service <div class='color1'>Manager</div><img src='img/save_green.png' class='img_icon'/></div>");
 		echo("</div>");
 
 		echo("<div id='leftpane_container' class='outline'>2");
 		echo("</div>");
 
-		echo("<div id='rightpane_container' class='outline'>3");
+		echo("<div id='rightpane_container' class='outline'>Company: " . $finalID);
 		echo("</div>");
 	}
 
@@ -36,27 +46,22 @@
 		");
 	}
 
-	function Action_CheckLogin($_email, $_password) {
-	}
-
 	if (isset($_POST['action'])) {
 		$action = $_POST['action'];
 		
 		switch ($action) {
 			case "CheckSession":
-				echo (Action_CheckSession());
+				Action_CheckSession($dbInfo);
 				break;
 			case "InitPortal":
-				Action_InitPortal();
+				Action_InitPortal($dbInfo);
 				break;
 			case "InitLogin":
 				Action_InitLogin();
 				break;
 			case "CheckLogin":
 				$_SESSION['email'] = $_POST['email'];
-				$_SESSION['password'] = $_POST['password']; 
-				$_SESSION['companyid'] = "1";
-				Action_CheckLogin($pEmail, $pPassword);
+				$_SESSION['password'] = $_POST['password'];
 				break;
 		}
 	}
