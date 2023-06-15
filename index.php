c0e112fa55f74c4cfcbd12e974d0c70214a6f683
seraphislabs <seraphislabs@gmail.com>
2023-06-15 00:39:46 +0000

    Css File Optimization

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
 <head>
 <link rel="icon" href="data:,">
 <link rel="stylesheet" type="text/css" href="styles/style.css" />
+<link rel="stylesheet" type="text/css" href="styles/popup.css" />
+<link rel="stylesheet" type="text/css" href="styles/buttons.css" />
+<link rel="stylesheet" type="text/css" href="styles/formsection.css" />
+<link rel="stylesheet" type="text/css" href="styles/accountview.css" />
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
 <script type="text/javascript" src="js/utils.js"></script>
 <script type="text/javascript" src="js/portal.js"></script>
